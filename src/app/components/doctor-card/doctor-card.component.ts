52b4dc95907dabf9e284c3a052e67f70f8b1e492
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-11-27 13:38:37 -0300

    fix: Fixed z-index bug

---
+++ src/app/components/doctor-card/doctor-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-card.component.html',
+  styleUrl: './doctor-card.component.css'
+})
+export class DoctorCardComponent {
+
+}
--- src/app/pages/find-a-doctor/find-a-doctor.component.ts
+++ src/app/pages/find-a-doctor/find-a-doctor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DoctorCardComponent } from '../../components/doctor-card/doctor-card.component';
 
 @Component({
   selector: 'app-find-a-doctor',
   standalone: true,
-  imports: [],
+  imports: [DoctorCardComponent],
   templateUrl: './find-a-doctor.component.html',
   styleUrl: './find-a-doctor.component.css'
 })
