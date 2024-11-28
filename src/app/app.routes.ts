f1b1ccdaf882de95122f08aec9e3f2fab91414c8
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-11-28 13:50:58 -0300

    Adds:Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { FindADoctorComponent } from './pages/find-a-doctor/find-a-doctor.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {
-        path: 'find-a-doctor',
-        component: FindADoctorComponent
-    }
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'find-a-doctor',
+    component: FindADoctorComponent,
+  },
 ];
