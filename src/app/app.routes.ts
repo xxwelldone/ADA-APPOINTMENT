2627d08c68b643a3c36104b8ae599848bc7c8d99
lidor <lidorbrosh@gmail.com>
2024-11-29 18:44:10 -0300

    Cria página de boas vindas e marcação de consultas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,16 @@ import { Routes } from '@angular/router';
 import { FindADoctorComponent } from './pages/find-a-doctor/find-a-doctor.component';
 import { MyAppointmentsComponent } from './pages/my-appointments/my-appointments.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'find-a-doctor',
-    component: FindADoctorComponent,
-  },
-   {
-        path: 'my-appointments',
-        component: MyAppointmentsComponent
-    }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'find-a-doctor', component: FindADoctorComponent },
+  { path: 'my-appointments', component: MyAppointmentsComponent },
 ];
