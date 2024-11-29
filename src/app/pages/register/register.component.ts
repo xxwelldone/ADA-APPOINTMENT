c94ceef9bb1457103eee3093368a585982f84675
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-11-29 17:06:28 -0300

    Adds Registration

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChildren } from '@angular/core';
 import {
   AbstractControl,
+  FormBuilder,
   FormControl,
   FormGroup,
   FormsModule,
@@ -11,7 +12,8 @@ import {
 } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -21,12 +23,12 @@ import { BehaviorSubject } from 'rxjs';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) {}
   warning = false;
   registerForm: FormGroup = new FormGroup({
     role: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     confirm: new FormControl('', [Validators.required]),
   });
@@ -37,8 +39,17 @@ export class RegisterComponent {
   }
 
   onRegister() {
-    console.log(this.registerForm);
+    const payload = this.formatPayload();
+    this.auth.register(payload).subscribe({
+      complete: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
+
   matchFieldsValidator = (field1: string, field2: string): ValidatorFn => {
     return (formGroup: AbstractControl): ValidationErrors | null => {
       const value1 = formGroup.get(field1)?.value;
@@ -52,4 +63,19 @@ export class RegisterComponent {
       return null;
     };
   };
+
+  formatPayload() {
+    const obj = this.registerForm.getRawValue();
+    const {
+      confirm,
+      ...payload
+    }: {
+      confirm: string;
+      name: string;
+      email: string;
+      password: string;
+      role: string;
+    } = obj;
+    return payload;
+  }
 }
