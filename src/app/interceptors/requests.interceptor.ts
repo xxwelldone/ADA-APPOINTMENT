c0d6fc7aaa9b57c4341b3599f95fbf757f2075ed
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-11-30 11:46:30 -0300

    Intercetor injecting token on request that are not auth

--- src/app/interceptors/requests.interceptor.ts
+++ src/app/interceptors/requests.interceptor.ts
@@ -1,5 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const requestsInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = sessionStorage.getItem('token');
+  const router = inject(Router);
+
+  if (req.url.includes('auth')) {
+    return next(req);
+  }
+  if (token !== null) {
+    const newReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(newReq);
+  }
+  router.navigate(['/login']);
   return next(req);
 };
