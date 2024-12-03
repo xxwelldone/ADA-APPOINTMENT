31958cf2be50fd820099ebd53d82ea35b7a90311
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-12-02 21:58:46 -0300

    Adds Appointment

--- src/app/components/doctor-card/doctor-card.component.ts
+++ src/app/components/doctor-card/doctor-card.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, Pipe } from '@angular/core';
+import { DoctorAppointment } from '../../models/doctor-appointment.model';
+import { CommonModule, DatePipe } from '@angular/common';
+import { AppointmentService } from '../../services/request/appointment.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, DatePipe],
   templateUrl: './doctor-card.component.html',
-  styleUrl: './doctor-card.component.css'
+  styleUrl: './doctor-card.component.css',
 })
 export class DoctorCardComponent {
+  @Input() DoctorDetails!: DoctorAppointment;
+  @Input() appointmentDate!: string;
 
+  availableTimes: string[] = [
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+  ];
+  time: string = '';
+
+  constructor(
+    private appointmentAPI: AppointmentService,
+    private router: Router
+  ) {}
+
+  isTimeAvailable(time: string) {
+    return this.DoctorDetails.appointments.some(
+      (appointment) =>
+        appointment.time === `${time}` || appointment.time === `${time}00`
+    );
+  }
+  OnTimeSelection(time: string) {
+    this.time = time;
+    this.saveAppointment();
+  }
+  saveAppointment() {
+    this.appointmentAPI
+      .post(this.DoctorDetails.id, this.appointmentDate, this.time)
+      .subscribe({
+        next: (respose) => {
+          console.log('Consulta agendada com sucesso');
+        },
+        complete: () => {
+          this.router.navigate(['/my-appointments']);
+        },
+        error: (err) => {
+          console.log(
+            'Houve erro ao salvar sua consulta no servidor, tente novamente mais tarde.'
+          );
+        },
+      });
+  }
 }
--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AppointmentService } from '../../services/request/appointment.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DoctorService } from '../../services/request/doctor.service';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { DoctorAppointment } from '../../models/doctor-appointment.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -17,9 +17,12 @@ import { Router } from '@angular/router';
 export class ScheduleComponent {
   constructor(private router: Router) {}
   public specialty!: string;
+  public date!: string;
   public doctors: DoctorAppointment[] = [];
 
   onChoose() {
-    this.router.navigate(['schedule/', this.specialty]);
+    if (this.date) {
+      this.router.navigate(['schedule/', `${this.specialty}`, this.date]);
+    }
   }
 }
